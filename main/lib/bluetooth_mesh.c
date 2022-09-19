82a6306a2618f0c68d12e88aa17d4da5c3065b40
francescogenovasi <francesco.genovasi@studenti.unisalento.it>
2022-09-19 10:27:13 +0200

    ripristino progetto originale per test

--- main/lib/bluetooth_mesh.c
+++ main/lib/bluetooth_mesh.c
@@ -146,8 +146,8 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                     ESP_LOGI(BLUETOOTH_MESH_TAG, "MESH MESSAGE SENT - MAJOR: %hu, MINOR: %d, RSSI: %d distance: %f - Counter #%d - \n",
                              ibeacon_resp.major, ibeacon_resp.minor, ibeacon_resp.rssi, ibeacon_resp.distance, ibeacon_resp.counter);
 
-                    // sovrascrivo per testare il provisoner
-                    uint8_t uuid_1[16] = {0x11, 0x22, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                    // TODO modificato da noi sovrascrivo per testare il provisoner
+                    /* uint8_t uuid_1[16] = {0x11, 0x22, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                                                   0x00, 0x00, 0x00, 0x00};
                     memcpy(ibeacon_resp.uuid, uuid_1, sizeof (uuid_1));
                     esp_log_buffer_hex("UUID SENT1: ", ibeacon_resp.uuid, ESP_UUID_LEN_128);
@@ -157,7 +157,9 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                     ibeacon_resp.distance = 444;
                     ibeacon_resp.counter = 555;
                     ESP_LOGI(BLUETOOTH_MESH_TAG, "MESH MESSAGE SENT1 - MAJOR: %hu, MINOR: %d, RSSI: %d distance: %f - Counter #%d - \n",
-                             ibeacon_resp.major, ibeacon_resp.minor, ibeacon_resp.rssi, ibeacon_resp.distance, ibeacon_resp.counter);
+                             ibeacon_resp.major, ibeacon_resp.minor, ibeacon_resp.rssi, ibeacon_resp.distance, ibeacon_resp.counter);*/
+
+
 
                     esp_err_t ib_err = esp_ble_mesh_server_model_send_msg(param->model_operation.model,
                                                                        param->model_operation.ctx,
@@ -175,7 +177,7 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                     /* TODO qui dovrebbe ricevere il segnale ma stampa 0 a uuid, major e minor
                      * mentre rssi funziona (lo calcola il server però)
                     */
-                    model_ibeacon_data_t ibeacon_response1;
+                    /* model_ibeacon_data_t ibeacon_response1;
                     uint8_t uuid_2[16] = {0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,
                                           0x99, 0x99, 0x99, 0x99};
                     memcpy(ibeacon_response1.uuid, uuid_2, sizeof (uuid_1));
@@ -188,13 +190,13 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                     ibeacon_response1 = *(model_ibeacon_data_t *)param->client_recv_publish_msg.model;
                     esp_log_buffer_hex("rcv test, UUID:", ibeacon_response1.uuid, 16);
                     ESP_LOGI("rcv test", "MAJOR: %hu, MINOR: %d, RSSI: %d distance: %f - Counter #%d -",
-                             ibeacon_response1.major, ibeacon_response1.minor, ibeacon_response1.rssi, ibeacon_response1.distance, ibeacon_response1.counter);
+                             ibeacon_response1.major, ibeacon_response1.minor, ibeacon_response1.rssi, ibeacon_response1.distance, ibeacon_response1.counter); */
 
 
                     model_ibeacon_data_t ibeaconData = *(model_ibeacon_data_t *) param->model_operation.model->user_data;
 
-                    esp_log_buffer_hex("received from beacon, UUID:", ibeaconData.uuid, 16);
-                    ESP_LOGI("received from beacon", "major: %d, minor: %d", ibeaconData.major, ibeaconData.minor);
+                    /* esp_log_buffer_hex("received from beacon, UUID:", ibeaconData.uuid, 16);
+                    ESP_LOGI("received from beacon", "major: %d, minor: %d", ibeaconData.major, ibeaconData.minor);*/
                     // rssi ok?????
                     update_ibeacon_state(ibeaconData.uuid, ibeaconData.major, ibeaconData.minor, rssi);
                 break;
@@ -218,11 +220,11 @@ void update_ibeacon_state(uint8_t *uuid, uint16_t major, uint16_t minor, int rss
     double envFactor = 4.6;
 
     memcpy(_ibeacon_model_state.uuid, uuid, 16);
-    esp_log_buffer_hex("update state: ", _ibeacon_model_state.uuid, ESP_UUID_LEN_128);
+    // esp_log_buffer_hex("update state: ", _ibeacon_model_state.uuid, ESP_UUID_LEN_128);
     _ibeacon_model_state.major = major;
     _ibeacon_model_state.minor = minor;
     _ibeacon_model_state.rssi = rssi;
     _ibeacon_model_state.distance = pow(10,((double )(meausuredPower - rssi) / (10 * envFactor)));
-    ESP_LOGI("update state", "%d %d %d", _ibeacon_model_state.major, _ibeacon_model_state.minor, _ibeacon_model_state.rssi);
+    // ESP_LOGI("update state", "%d %d %d", _ibeacon_model_state.major, _ibeacon_model_state.minor, _ibeacon_model_state.rssi);
     _ibeacon_model_state.counter++;
 }
