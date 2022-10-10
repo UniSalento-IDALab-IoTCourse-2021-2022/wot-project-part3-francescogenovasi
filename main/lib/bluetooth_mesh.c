59fcc1929f0275d3b12e7d34fbf5d84646e9816a
francescogenovasi <francesco.genovasi@studenti.unisalento.it>
2022-10-10 15:04:57 +0200

    last_2

--- main/lib/bluetooth_mesh.c
+++ main/lib/bluetooth_mesh.c
@@ -141,12 +141,11 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                     model_ibeacon_data_t ibeacon_resp = *(model_ibeacon_data_t *) param->model_operation.model->user_data;
 
                     // TODO dati ricevuti da beacon e invio al provisoner
-                    // ricevuti
-                    /* TODO commento calibrazione esp_log_buffer_hex("UUID: ", ibeacon_resp.uuid, ESP_UUID_LEN_128);
+                    /* esp_log_buffer_hex("UUID: ", ibeacon_resp.uuid, ESP_UUID_LEN_128);
                     ESP_LOGI(BLUETOOTH_MESH_TAG, "MESH MESSAGE SENT - MAJOR: %hu, MINOR: %d, RSSI: %d distance: %f - Counter #%d - \n",
                              ibeacon_resp.major, ibeacon_resp.minor, ibeacon_resp.rssi, ibeacon_resp.distance, ibeacon_resp.counter); */
 
-                    // TODO modificato da noi sovrascrivo per testare il provisoner
+                    // sovrascrivere per testare il provisoner
                     /* uint8_t uuid_1[16] = {0x11, 0x22, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                                                   0x00, 0x00, 0x00, 0x00};
                     memcpy(ibeacon_resp.uuid, uuid_1, sizeof (uuid_1));
@@ -166,9 +165,7 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                                                                        ESP_BLE_MESH_IBEACON_MODEL_OP_STATUS,
                                                                        sizeof(ibeacon_resp), (uint8_t *) &ibeacon_resp);
                     if (ib_err) {
-                        /* TODO commento calibrazione
-                        ESP_LOGE(BLUETOOTH_MESH_TAG, "Failed to send message 0x%06x",
-                                 ESP_BLE_MESH_IBEACON_MODEL_OP_STATUS);*/
+                        ESP_LOGE(BLUETOOTH_MESH_TAG, "Failed to send message 0x%06x", ESP_BLE_MESH_IBEACON_MODEL_OP_STATUS);
                     }
                     break;
                 case ESP_BLE_MESH_IBEACON_MODEL_OP_BEACON:;
@@ -186,18 +183,18 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
                     ibeacon_response1.minor = 999;
                     ibeacon_response1.rssi = 999;
                     ibeacon_response1.distance = 999;
-                    ibeacon_response1.counter = 999;
+                    ibeacon_response1.counter = 999;*/
 
-                    ibeacon_response1 = *(model_ibeacon_data_t *)param->client_recv_publish_msg.model;
+                    /* model_ibeacon_data_t ibeacon_response1 = *(model_ibeacon_data_t *)param->client_recv_publish_msg.model;
                     esp_log_buffer_hex("rcv test, UUID:", ibeacon_response1.uuid, 16);
                     ESP_LOGI("rcv test", "MAJOR: %hu, MINOR: %d, RSSI: %d distance: %f - Counter #%d -",
                              ibeacon_response1.major, ibeacon_response1.minor, ibeacon_response1.rssi, ibeacon_response1.distance, ibeacon_response1.counter); */
 
-
                     model_ibeacon_data_t ibeaconData = *(model_ibeacon_data_t *) param->model_operation.model->user_data;
 
                     /* esp_log_buffer_hex("received from beacon, UUID:", ibeaconData.uuid, 16);
-                    ESP_LOGI("received from beacon", "major: %d, minor: %d", ibeaconData.major, ibeaconData.minor);*/
+                    ESP_LOGI("received from beacon", "major: %d, minor: %d", ibeaconData.major, ibeaconData.minor); */
+
                     // rssi ok?????
                     update_ibeacon_state(ibeaconData.uuid, ibeaconData.major, ibeaconData.minor, rssi);
                 break;
@@ -211,14 +208,14 @@ void custom_ibeacon_server_callback(esp_ble_mesh_model_cb_event_t event, esp_ble
 }
 
 void update_ibeacon_state(uint8_t *uuid, uint16_t major, uint16_t minor, int rssi) {
-    /* d = 10^(((P)-(S))/(10*N))
+    /* d = 10^(((A)-(S))/(10*N))
      Where:
         d - estimated distance in meters
-        P - beacon broadcast power in dBm at 1 m (Tx Power)
+        A - beacon broadcast power in dBm at 1 m (Tx Power)
         S - measured signal value (RSSI) in dBm
         N - environmental factor (usually value between 2 and 4) */
-    int meausuredPower = -70;
-    double envFactor = 4.15;
+    int meausuredPower = -71;
+    double envFactor = 3.3;
 
     memcpy(_ibeacon_model_state.uuid, uuid, 16);
     // esp_log_buffer_hex("update state: ", _ibeacon_model_state.uuid, ESP_UUID_LEN_128);
